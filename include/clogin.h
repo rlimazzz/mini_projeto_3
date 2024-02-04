85d6463e32e4a92188c073459f2dbd3ec5ae1d34
C4I0 <107209603+caio-farias21@users.noreply.github.com>
2024-02-04 19:16:31 +0000

    final

--- include/clogin.h
+++ include/clogin.h
@@ -3,21 +3,18 @@
 
 // the clogin project header
 
-// including an unreasonable
-// amount of libraries:
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 #include <ctype.h>
 #include <time.h>
+#include "clogin_memanager.h"
 
-#define MAX_BUFFER 10000
-#define MAX_FIELD 255
-#define DURACAO_SESSAO 60 * 60 * 24 * 7 // 1 semana
 #define FILE_SEP ','
 #define CAPTCHA_LENGTH 5
 
+int DURACAO_SESSAO = 60 * 60 * 24 * 7;
 char databasePath[MAX_FIELD];
 bool databasePathSet = false;
 
@@ -31,18 +28,25 @@ typedef struct {
     char *email;
     char *username;
     int _expira_em;
+    bool _has;
 } Sessao;
 
-bool check_character(char * str, char key) {
+bool CheckCharacter(char * str, char key) {
 	return memchr(str, key, strlen(str)) != NULL;
 }
 
-void setDatabasePath(char * path) {
+Sessao SessaoFactory() {
+    Sessao sessao;
+    sessao._has = false;
+    return sessao;
+}
+
+void SetDatabasePath(char * path) {
 	strcpy(databasePath, path);
 
 	FILE * file = fopen(databasePath, "rb");
 	if (file == NULL) {
-		printf("setDatabasePath falhou, caminho do arquivo não foi encontrado!\n");
+		printf("SetDatabasePath falhou, caminho do arquivo não foi encontrado!\n");
 		exit(EXIT_FAILURE);
 	}
 
@@ -50,10 +54,54 @@ void setDatabasePath(char * path) {
 	databasePathSet = true;
 }
 
+void SetDuracaoSessao(int novaDuracao) {
+    if (novaDuracao < 0) {
+        printf("Duração de sessão inválida: %d\n", novaDuracao);
+        return;
+    }
+    DURACAO_SESSAO = novaDuracao;
+}
+
+User * _ProcurarUsuario(char * username, char * password, bool verificarSenha) {
+
+    FILE * file = fopen(databasePath, "rb");
+	if(file == NULL) {
+        printf("Memory allocation error");
+        return NULL;
+    }
+
+    char Fusername[MAX_FIELD], Fpassword[MAX_FIELD], Femail[MAX_FIELD];
+    while (fscanf(file, "%[^,],%[^,],%[^\n]\n", Fusername, Fpassword, Femail) != EOF) {
+        if (strcmp(Fusername, username) == 0 && (!verificarSenha || strcmp(Fpassword, password) == 0)) {
+            
+            fclose(file);
+
+            User *user = (User*) malloc(sizeof(User));
+            if (user == NULL) {
+                printf("Memory alloc error (_ProcurarUsuario)\n");
+                return NULL;
+            }
+
+            Cleaner userCleaner = CleanerFactory(3);
+
+            FileCharAlloc(&user->email, &userCleaner, file);
+            FileCharAlloc(&user->username, &userCleaner, file);
+            
+            strcpy(user->email, Femail);
+            strcpy(user->username, Fusername);
+            user->password = NULL;
+
+            return user;
+        }
+    }
+
+    return NULL;
+}
+
 bool cadastro() {
 
 	if (!databasePathSet) {
-		printf("Database path not defined! Use 'setDatabasePath' to set the path to your database\n");
+		printf("Database path not defined! Use 'SetDatabasePath' to set the path to your database\n");
 		exit(EXIT_FAILURE);
 	}
 
@@ -62,19 +110,13 @@ bool cadastro() {
         printf("Memory allocation error or file not exits");
         exit(EXIT_FAILURE);
     }
-	
+
+    Cleaner cleaner = CleanerFactory(3);
 	User cadastrando;
-	cadastrando.username = (char*) malloc(100*sizeof(char));
-	if (cadastrando.username == NULL)
-		return false;
 
-	cadastrando.password = (char*) malloc(100*sizeof(char));
-	if (cadastrando.password == NULL)
-		return false;
-
-	cadastrando.email = (char*) malloc(300*sizeof(char));
-	if (cadastrando.email == NULL)
-		return false;
+    FileCharAlloc(&cadastrando.username, &cleaner, file);
+    FileCharAlloc(&cadastrando.password, &cleaner, file);
+    FileCharAlloc(&cadastrando.email, &cleaner, file);
 
 	printf("\nREALIZAÇÃO DO CADASTRO\n");
 
@@ -88,13 +130,20 @@ bool cadastro() {
 	scanf("%s%*c", cadastrando.email);
 
 	if (
-		check_character(cadastrando.username, FILE_SEP)
-		|| check_character(cadastrando.password, FILE_SEP)
-		|| check_character(cadastrando.email, FILE_SEP)
+		CheckCharacter(cadastrando.username, FILE_SEP)
+		|| CheckCharacter(cadastrando.password, FILE_SEP)
+		|| CheckCharacter(cadastrando.email, FILE_SEP)
 	) {
 		printf("CARACTER INVÁLIDO INSERIDO '%c'!\n", FILE_SEP);
+        FileClean(&cleaner, file);
 		return false;
 	}
+
+    User * searchDuplicate = _ProcurarUsuario(cadastrando.username, cadastrando.password, false);
+    if (searchDuplicate != NULL) {
+        printf("USUÁRIO JÁ CADASTRADO!\n");
+        return false;
+    }
 	
     char mensagem[MAX_BUFFER];
     sprintf(mensagem, "%s%c%s%c%s\n", 
@@ -104,46 +153,31 @@ bool cadastro() {
 		FILE_SEP,
 		cadastrando.email
 	);
+
     fwrite(mensagem, strlen(mensagem), 1, file);
-	
 	printf("CADASTRO REALIZADO COM SUCESSO: %s \n", cadastrando.username);
 
-	free(cadastrando.username);
-	free(cadastrando.password);
-	free(cadastrando.email);	
+    FileClean(&cleaner, file);
+    return true;
 }
 
 Sessao login(int maxTries) {
 
 	if (maxTries == 0) {
-		printf("\nMaximum number of attempts reached, please try again later\n");
+		printf("Número máximo de tentativas atingidas, por favor, tente novamente mais tarde\n");
 		exit(EXIT_FAILURE);
 	}
 
-	Sessao sessao;
 	if (!databasePathSet) {
-		printf("Database path not defined! Use 'setDatabasePath' to set the path to your database\n");
+		printf("Database path not defined! Use 'SetDatabasePath' to set the path to your database\n");
 		exit(EXIT_FAILURE);
 	}
 
-	FILE * file = fopen(databasePath, "rb");
-	if(!file) {
-        printf("Memory allocation error");
-        exit(EXIT_FAILURE);
-    }
-
+    Cleaner visitanteCleaner = CleanerFactory(2);
 	User visitante;
-	visitante.username = (char*) calloc(100, sizeof(char));
-    if (visitante.username == NULL) {
-        printf("Memory allocation error");
-        exit(EXIT_FAILURE);
-    }
 
-	visitante.password = (char*) calloc(100, sizeof(char));
-    if (visitante.password == NULL) {
-        printf("Memory allocation error");
-        exit(EXIT_FAILURE);
-    }
+    CharAlloc(&visitante.username, &visitanteCleaner);
+    CharAlloc(&visitante.password, &visitanteCleaner);
 
 	printf("Insira seu username: ");
 	scanf("%s%*c", visitante.username);
@@ -151,41 +185,28 @@ Sessao login(int maxTries) {
 	printf("Insira sua senha: ");
 	scanf("%s%*c", visitante.password);
 
-    char username[100], password[100], email[MAX_FIELD];
-    while (fscanf(file, "%[^,],%[^,],%[^\n]\n", username, password, email) != EOF) 
-	{
-        if (strcmp(username, visitante.username) == 0 && strcmp(password, visitante.password) == 0) 
-		{
-        	fclose(file);
-
-            sessao.email = (char*) malloc(sizeof(char) * strlen(email));
-            if (sessao.email == NULL) {
-                printf("Memory allocation error");
-                exit(EXIT_FAILURE);
-            }
-
-            sessao.username = (char*) malloc(sizeof(char) * strlen(username));
-            if (sessao.username == NULL) {
-                printf("Memory allocation error");
-                exit(EXIT_FAILURE);
-            }
-
-            free(visitante.username);
-            free(visitante.password);
-
-            strcpy(sessao.email, email);
-            strcpy(sessao.username, username);
-            sessao._expira_em = time(NULL) + DURACAO_SESSAO;
-
-            return sessao;
-        }
+    User * databaseUser = _ProcurarUsuario(visitante.username, visitante.password, true);
+    if (databaseUser == NULL) {
+        Clean(&visitanteCleaner);
+        printf("\033[1;31mCredenciais inválidas.\033[0m\n\n");
+        return login(maxTries - 1);
     }
 
-    fclose(file);
-    free(visitante.username);
-    free(visitante.password);
+    Sessao sessao = SessaoFactory();
+    Cleaner sessaoCleaner = CleanerFactory(2);
 
-    return login(maxTries - 1);
+    CharAlloc(&sessao.email, &sessaoCleaner);
+    CharAlloc(&sessao.username, &sessaoCleaner);
+
+    Clean(&visitanteCleaner);
+
+    strcpy(sessao.email, databaseUser->email);
+    strcpy(sessao.username, databaseUser->username);
+
+    sessao._expira_em = time(NULL) + DURACAO_SESSAO;
+    sessao._has = true;
+
+    return sessao;
 }
 
 char *generate_captcha() {
@@ -212,23 +233,19 @@ char *generate_captcha() {
 }
 
 bool captcha() {
-    system("clear");
-
     int i, n;
-    time_t t;
     n = 5;
     srand(time(0));
 
     for (i = 0; i < n; i++) {
         char* generated_captcha = generate_captcha();
-        printf("DIGITE OS CARACTERES APRESENTADOS: %s\n", generated_captcha);
+        printf("DIGITE OS CARACTERES APRESENTADOS: \033[1;35m%s\033[0m\n", generated_captcha);
 
         char user_input[CAPTCHA_LENGTH + 1];
         scanf("%5s", user_input);
         int c; while((c = getchar()) != '\n' && c != EOF) {}
 
         if (strcmp(user_input, generated_captcha) == 0) {
-            printf("CAPTCHA CORRETO!\n");
             return true;
         } else {
             printf("CAPTCHA INCORRETO, TENTE NOVAMENTE!\n");
@@ -240,16 +257,33 @@ bool captcha() {
     return false;
 }
 
-Sessao validarSessao(Sessao sessao, int maxTries) {
+bool sessaoExpirou(Sessao sessao) {
     time_t agora = time(NULL);
+    return sessao._expira_em < agora;
+}
 
-    if (sessao._expira_em < agora) {
-        printf("Sua sessão foi expirada, por favor faça login novamente!\n\n");
+Sessao validarSessao(Sessao sessao, int maxTries) {
+
+    if (!sessao._has || sessaoExpirou(sessao)) {
+        printf("Sua sessão é inválida ou foi expirada, por favor faça login novamente!\n\n");
         Sessao novaSessao = login(maxTries);
         return novaSessao;
     };
 
     return sessao;
+}
+
+int verificarDuracaoSessao(Sessao sessao) {
+    if (!sessao._has) {
+        printf("Verificação de duração de sessão falhou!\n");
+        return 0;
+    }
+    else if (sessaoExpirou(sessao)) {
+        printf("Sessão expirada!\n");
+        return 0;
+    }
+    
+    return sessao._expira_em - time(NULL);
 }
 
 #endif 
--- tests/main.c
+++ tests/main.c
@@ -1,23 +1,65 @@
 #include "../include/clogin.h"
 #include <unistd.h>
-#define DATABASE_TEST_PATH "./teste.bin"
+#define DATABASE_TEST_PATH "./tests/teste.dat" // Executado a partir da raiz
 
 void assert(bool expression, char * msg);
 void removeLastLine();
 
 int main() {
-    setDatabasePath(DATABASE_TEST_PATH);
+    SetDatabasePath(DATABASE_TEST_PATH);
 
-    cadastro();
-    Sessao sessao = login(3);
+    bool primeiroCadastroSucedido = false;
+    bool segundoCadastroComMesmaCredencialFalha = false;
+    bool usuarioNaBaseDeDadosAposCadastro = false;
+    bool retornaFalsoAoInserirCredenciaisComCaracterInvalido = false;
+    bool duracaoSessaoInvalidaNegativa = false;
+    bool duracaoSessaoValidaPositiva = false;
+
+    // Duração de sessao
+    SetDuracaoSessao(-10);
+    duracaoSessaoInvalidaNegativa = DURACAO_SESSAO != -10;
+    SetDuracaoSessao(10);
+    duracaoSessaoValidaPositiva = DURACAO_SESSAO == 10;
+
+    // Cadastros
+    primeiroCadastroSucedido = cadastro();
+    User * usuarioTeste = _ProcurarUsuario("usernameTeste", "senhaTeste", true);
+    if (usuarioTeste != NULL)
+        usuarioNaBaseDeDadosAposCadastro = true;
+    
+    segundoCadastroComMesmaCredencialFalha = !cadastro();
+    retornaFalsoAoInserirCredenciaisComCaracterInvalido = !cadastro();
+
+    // Login e sessão
+    Sessao sessao = login(1);
+    int offset_sessao = sessao._expira_em - time(NULL);
+    int test_offset_sessao = verificarDuracaoSessao(sessao);
+
 
     removeLastLine();
 
+    fflush(stdout);
+    system("clear");
+
+    printf("\n--------- Iniciando testes ---------\n");
+
+    assert(usuarioNaBaseDeDadosAposCadastro, "Função procurar usuário encontrou credenciais de teste");
+    assert(primeiroCadastroSucedido, "Cadastro retornou true para tentativa válida de cadastro");
+    assert(segundoCadastroComMesmaCredencialFalha, "Cadastro falha para mesmo tentativa de cadastro duplicado");
+    assert(retornaFalsoAoInserirCredenciaisComCaracterInvalido, "Cadastro falha ao informar em algum campo caracter de separação da base de dados");
+
+    assert(sessao._has, "Parâmetro de sessao ativa muda para true");
     assert(strcmp(sessao.email, "teste@teste.com") == 0, "Email correto ao retornar sessao");
     assert(strcmp(sessao.username, "usernameTeste") == 0, "Nome de usuário correto ao retornar sessao");
-
-    int offset_sessao = sessao._expira_em - time(NULL);
+    assert(test_offset_sessao >= 0 && test_offset_sessao <= offset_sessao, "Contagem de segundos correta na sessao");
     assert(offset_sessao >= 0 && offset_sessao <= DURACAO_SESSAO, "Duração de sessão correta ao retornar sessao");
+
+    assert(strlen(generate_captcha()) == CAPTCHA_LENGTH, "Captcha é gerado com tamanho correto");
+    assert(strcmp(DATABASE_TEST_PATH, databasePath) == 0, "Caminho da base de dados correto");
+    assert(duracaoSessaoInvalidaNegativa, "SetDuracao impede alteracao para duração de sessao negativa");
+    assert(duracaoSessaoValidaPositiva, "SetDuracao altera corretamente a duração de sessão");
+
+    printf("\n\n--------- Testes finalizados ---------\n\n");
 }
 
 void removeLastLine() {
@@ -47,8 +89,8 @@ void removeLastLine() {
 
 void assert(bool expression, char * msg) {
     if (expression) {
-        printf("\n\033[1;32mSucesso - %s\033[0m\n", msg);
+        printf("\n\033[1;32mSucesso - %s\033[0m", msg);
         return;
     }
-    printf("\n\033[1;31mFalha - %s\033[0m\n", msg);
+    printf("\n\033[1;31mFalha - %s\033[0m", msg);
 }
